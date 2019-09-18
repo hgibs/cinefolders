2589e66d70823b4908a26aa2fa3c6145bb2a6a84
ArcAwe-MBP <holland.gibson@gmail.com>
2019-09-18 12:42:40 -0400

    think i got it now

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cinefolders",
-    version="0.0.5",
+    version="0.0.6",
     author="Holland Gibson",
     author_email="cinefiles-hgibs@googlegroups.com",
     description="A utility for organizing a media folder",
