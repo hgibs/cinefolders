e8f39398213d6a5539c671b85cee8edb85bde720
ArcAwe-MBP <holland.gibson@gmail.com>
2019-09-18 12:37:11 -0400

    changing git commit defaults

--- dev/bump-publish.sh
+++ dev/bump-publish.sh
@@ -9,11 +9,14 @@ OLDVERSION="$(bump2version --dry-run --list $PART | grep current_version | awk '
 NEWVERSION="$(bump2version --dry-run --list $PART | grep new_version | awk '{split($0,a,"="); print "v"a[2]}' )"
 
 bump2version $PART
+
 echo -e "\n\n$OLDVERSION → $NEWVERSION" > dev/git-template.txt
 
+nano dev/git-template.txt
+
 git add .
-git commit -t dev/git-template.txt
-git tag
+git commit -F dev/git-template.txt
+git tag -a $NEWVERSION -F dev/git-template.txt
 git push --tags
 
 python3 setup.py bdist_wheel sdist
