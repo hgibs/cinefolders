0cda00e6353061e125440075a4a93206b70139c1
Holland Gibson <holland.gibson@gmail.com>
2020-05-20 21:25:38 -0400

    Removed x-rate-limit header request due to TMDb removing it'

--- src/cinefolders/tmdb/tmdb.py
+++ src/cinefolders/tmdb/tmdb.py
@@ -89,23 +89,26 @@ class TMDb:
     def getmovie(self, id):
         idnum = int(id)
         return movie.Movie({'id':idnum},self)
-        
+
+
+    #TODO: tmdb has removed rate limiting as of dec 16, 2019
     def safeapi(self, url, callnum=0):
-#         now = floor(time())
-        sleepdelta = self.safetime-time()
-        if(sleepdelta > 0):
-            #wait until safe to start API access again
-            logging.info("TMDb API is reaching rate limit!")
-            sleep(sleepdelta)
+        now = floor(time())
+        # sleepdelta = self.safetime-time()
+        # if(sleepdelta > 0):
+        #     #wait until safe to start API access again
+        #     logging.info("TMDb API is reaching rate limit!")
+        #     sleep(sleepdelta)
         thisrequest = requests.get(url)
         if(thisrequest.ok):
-            self.safetime = self.checktime(thisrequest)
-            if(self.getremainingreqs(thisrequest) < 3):
-                #with small buffer, set wait-out until rate-limit clears
-                self.safetime = self.getresettime(thisrequest)
-        elif(thisrequest.status_code == 429 and callnum < 3):
-            self.sleepuntil(self.getresettime(thisrequest))
-            return self.safeapi(url,1)
+            return thisrequest
+            # self.safetime = self.checktime(thisrequest)
+            # if(self.getremainingreqs(thisrequest) < 3):
+            #     #with small buffer, set wait-out until rate-limit clears
+            #     self.safetime = self.getresettime(thisrequest)
+        # elif(thisrequest.status_code == 429 and callnum < 3):
+        #     self.sleepuntil(self.getresettime(thisrequest))
+        #     return self.safeapi(url,1)
         else:
             tdata = json.loads(thisrequest.text)
             if('status_code' in tdata):
@@ -116,45 +119,45 @@ class TMDb:
         
         return thisrequest
             
-    def checktime(self, request):
-        #returns the time the next request should wait to before starting
-        #make sure we aren't exceeding our rate limit of 40 requests / 10 seconds
-        now = floor(time())
-        resettime = self.getresettime(request)
-        secondsrem = (resettime-now)
-        #average 4/second
-        reqsrem = self.getremainingreqs(request)
-        if(secondsrem > 0):
-            reqratio = reqsrem/secondsrem
-            if(reqsrem <= 1):
-                #be safe and wait it out
-                return float(request.headers['X-RateLimit-Reset'])
-            if(reqratio < 4.0):
-                #wait until average is back up to 4.0/sec
-                sleepdelta = secondsrem-(reqsrem-1)/reqratio
-                return now+sleepdelta
-            else:
-                #ratio is good
-                return now
-        else:
-            #0 seconds remaining
-            return now
+    # def checktime(self, request):
+    #     #returns the time the next request should wait to before starting
+    #     #make sure we aren't exceeding our rate limit of 40 requests / 10 seconds
+    #     now = floor(time())
+    #     # resettime = self.getresettime(request)
+    #     # secondsrem = (resettime-now)
+    #     # #average 4/second
+    #     # reqsrem = self.getremainingreqs(request)
+    #     # if(secondsrem > 0):
+    #     #     reqratio = reqsrem/secondsrem
+    #     #     if(reqsrem <= 1):
+    #     #         #be safe and wait it out
+    #     #         return float(request.headers['X-RateLimit-Reset'])
+    #     #     if(reqratio < 4.0):
+    #     #         #wait until average is back up to 4.0/sec
+    #     #         sleepdelta = secondsrem-(reqsrem-1)/reqratio
+    #     #         return now+sleepdelta
+    #     #     else:
+    #     #         #ratio is good
+    #     #         return now
+    #     # else:
+    #     #     #0 seconds remaining
+    #     #     return now
             
-    def getresettime(self, request):
-        if('X-RateLimit-Reset' in request.headers):
-            return float(request.headers['X-RateLimit-Reset'])
-        else:
-            tdata = json.loads(request.text)
-            if('Retry-After' in tdata):
-                return time()+float(tdata['Retry-After'])
-            else:
-                return time()+10.0
+    # def getresettime(self, request):
+    #     if('X-RateLimit-Reset' in request.headers):
+    #         return float(request.headers['X-RateLimit-Reset'])
+    #     else:
+    #         tdata = json.loads(request.text)
+    #         if('Retry-After' in tdata):
+    #             return time()+float(tdata['Retry-After'])
+    #         else:
+    #             return time()+10.0
         
-    def getremainingreqs(self, request):
-        if('X-RateLimit-Remaining' in request.headers):
-            return float(request.headers['X-RateLimit-Remaining'])
-        else:
-            return 0
+    # def getremainingreqs(self, request):
+    #     if('X-RateLimit-Remaining' in request.headers):
+    #         return float(request.headers['X-RateLimit-Remaining'])
+    #     else:
+    #         return 0
         
     def sleepuntil(self, endtime):
         delta = endtime-time()
