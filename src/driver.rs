0a782b24425705eacc46969e0fb5f167e723e7ee
Holland Gibson <holland.gibson@gmail.com>
2022-12-24 09:41:33 -0600

    adding more stability

--- src/driver.rs
+++ src/driver.rs
@@ -17,18 +17,18 @@ pub enum SearchError {
     NotFound,
 }
 
-pub fn drive(sysconfig: Config) -> Result<u32, SearchError> {
+#[tokio::main]
+pub async fn drive(sysconfig: Config) -> Result<u32, SearchError> {
     // drive should only run once, hence unwrap ok:
     AUTH_KEY.set(sysconfig.auth_token).unwrap();
 
     let tmdb = Client::new(auth_key().to_string());
-    search_one(&tmdb);
+    search_one(&tmdb).await;
 
     Ok(0)
 }
 
 
-#[tokio::main]
 async fn search_one(client: &Client) {
     // let search_result = client.movie_search("Interstellar", Some(2014)).await.unwrap();
     let cmd = TVShowSearch::new("simpsons".into());
